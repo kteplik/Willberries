6f61225add8b0cd0ccd9708a0921b201ef9ed280
kteplik <k.teplik@gmail.com>
2022-03-17 20:25:08 +0200

    step 1 complete

--- js/search.js
+++ js/search.js
@@ -1,10 +1,20 @@
 const search = function () {
     const input = document.querySelector('.search-block > input')
-    const searchBtn = document.querySelector('.search-block > button')
+    const searchBtn = document.getElementById('button-addon2')
 
+    /*
+    console.log(searchBtn)
 
     input.addEventListener('input', (event) => {
         console.log(event.target.value)
+    })
+
+    input.addEventListener('mouseup', function (event) {
+        console.log(event.target.value)
+    })
+    */
+    searchBtn.addEventListener('click', () => {
+        console.log(input.value)
     })
 }
 
